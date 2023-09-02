efa24082cdb0718fad37c0b47f1cf92f8d1a87af
JTRB <jacobrobertsbaca@gmail.com>
2023-09-01 23:35:38 -0600

    feat: update routes w/ placeholder

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import { Box, Container, Stack, Typography } from '@mui/material';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+
+const Page = () => (
+  <>
+    <Head>
+      <title>
+        Budgets | zero
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="lg">
+        <Stack spacing={3}>
+          <Typography variant="h4">
+            Budgets
+          </Typography>
+        </Stack>
+      </Container>
+    </Box>
+  </>
+);
+
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Page;
--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,16 +1,11 @@
 import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
 import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
+import CreditCardIcon from '@heroicons/react/24/solid/CreditCardIcon';
 import { SvgIcon } from '@mui/material';
 
 export const items = [
   {
-    title: 'Overview',
+    title: 'Budgets',
     path: '/',
     icon: (
       <SvgIcon fontSize="small">
@@ -19,29 +14,11 @@ export const items = [
     )
   },
   {
-    title: 'Customers',
-    path: '/customers',
+    title: 'Transactions',
+    path: '/transactions',
     icon: (
       <SvgIcon fontSize="small">
-        <UsersIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Companies',
-    path: '/companies',
-    icon: (
-      <SvgIcon fontSize="small">
-        <ShoppingBagIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Account',
-    path: '/account',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserIcon />
+        <CreditCardIcon />
       </SvgIcon>
     )
   },
@@ -51,33 +28,6 @@ export const items = [
     icon: (
       <SvgIcon fontSize="small">
         <CogIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Login',
-    path: '/auth/login',
-    icon: (
-      <SvgIcon fontSize="small">
-        <LockClosedIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Register',
-    path: '/auth/register',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserPlusIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Error',
-    path: '/404',
-    icon: (
-      <SvgIcon fontSize="small">
-        <XCircleIcon />
       </SvgIcon>
     )
   }
