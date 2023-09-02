e282fc9216f8db42c877706f71f920fe14447a97
JTRB <jacobrobertsbaca@gmail.com>
2023-09-01 21:57:35 -0600

    feat: change to devias-io/material-kit-react

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 - Recommended `node js 16.x` and `npm 6+`. (suggestion v16.15.0)
 - Install dependencies: `npm install`
-- Start the project: `npm run start`
+- Start the project: `npm run dev`
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,44 +1,31 @@
-import PropTypes from 'prop-types';
-import { useMemo } from 'react';
-// @mui
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider as MUIThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
-//
-import palette from './palette';
-import shadows from './shadows';
-import typography from './typography';
-import GlobalStyles from './globalStyles';
-import customShadows from './customShadows';
-import componentsOverride from './overrides';
+import { createTheme as createMuiTheme } from '@mui/material';
+import { createPalette } from './create-palette';
+import { createComponents } from './create-components';
+import { createShadows } from './create-shadows';
+import { createTypography } from './create-typography';
 
-// ----------------------------------------------------------------------
+export function createTheme() {
+  const palette = createPalette();
+  const components = createComponents({ palette });
+  const shadows = createShadows();
+  const typography = createTypography();
 
-ThemeProvider.propTypes = {
-  children: PropTypes.node,
-};
-
-export default function ThemeProvider({ children }) {
-  const themeOptions = useMemo(
-    () => ({
-      palette,
-      shape: { borderRadius: 6 },
-      typography,
-      shadows: shadows(),
-      customShadows: customShadows(),
-    }),
-    []
-  );
-
-  const theme = createTheme(themeOptions);
-  theme.components = componentsOverride(theme);
-
-  return (
-    <StyledEngineProvider injectFirst>
-      <MUIThemeProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles />
-        {children}
-      </MUIThemeProvider>
-    </StyledEngineProvider>
-  );
+  return createMuiTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1440
+      }
+    },
+    components,
+    palette,
+    shadows,
+    shape: {
+      borderRadius: 8
+    },
+    typography
+  });
 }
