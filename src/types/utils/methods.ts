eabc561f24d9c768e3e27bce06a2ee82cd9e3612
JTRB <jacobrobertsbaca@gmail.com>
2023-09-09 18:33:14 -0600

    feat: DateString
    - Converting dates to string format to help with serialization (avoiding use of Date in core data structures)

--- src/types/utils/methods.ts
+++ src/types/utils/methods.ts
@@ -1,5 +1,56 @@
 import { produce } from "immer";
-import { Dates } from "./types";
+import { DateString, Dates } from "./types";
+import { isString } from "lodash";
+
+/* ================================================================================================================= *
+ * Utilities                                                                                                         *
+ * ================================================================================================================= */
+
+
+/* ================================================================================================================= *
+ * DateString                                                                                                        *
+ * ================================================================================================================= */
+
+export const isDateString = (date: unknown): date is DateString => isString(date) && date.length === 8;
+
+/**
+ * Converts a {@link Date} or {@link DateString} to a date.
+ * @param date A {@link Date} or {@link DateString}. Will not be modified.
+ * @param dayOffset Adds this many days to the result.
+ * @returns A {@link Date}. Its hours, minutes, seconds, and milliseconds are guaranteed to be zero.
+ */
+export const asDate = (date: Date | DateString, dayOffset: number = 0): Date => {
+  if (isDateString(date)) {
+    const years = parseInt(date.substring(0, 4), 10);
+    const months = parseInt(date.substring(4, 6), 10) - 1;
+    const days = parseInt(date.substring(6, 8), 10) + dayOffset;
+    return new Date(years, months, days);
+  }
+
+  date = new Date(date);
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + dayOffset);
+  return date;
+};
+
+/**
+ * Converts a {@link Date} or {@link DateString} to a {@link DateString}.
+ * @param date A {@link Date} or {@link DateString}. Will not be modified.
+ * @param dayOffset Adds this many days to the result.
+ * @returns A {@link DateString}
+ */
+export const asDateString = (date: Date | DateString, dayOffset: number = 0): DateString => {
+  date = asDate(date, dayOffset);
+  const years = `${date.getFullYear()}`.padStart(4, '0');
+  const months = `${date.getMonth() + 1}`.padStart(2, '0');
+  const days = `${date.getDay()}`.padStart(2, '0');
+  return `${years}${months}${days}`;
+};
+
+/* ================================================================================================================= *
+ * Date Ranges                                                                                                       *
+ * ================================================================================================================= */
+
 
 export const fixDate = (date: Date, dayOffset: number = 0): Date => {
   const result = new Date(date);
--- src/types/utils/types.ts
+++ src/types/utils/types.ts
@@ -1,3 +1,7 @@
 import { Immutable } from "immer";
 
+/**
+ * A date in the format YYYYMMDD
+ */
+export type DateString = string;
 export type Dates = Immutable<{ begin: Date; end: Date }>;
