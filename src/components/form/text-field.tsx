0485f78778960e851238e2f4ef0940026496ed3e
JTRB <jacobrobertsbaca@gmail.com>
2023-09-19 00:11:11 -0600

    fix: extraneous log

--- src/components/form/text-field.tsx
+++ src/components/form/text-field.tsx
@@ -9,7 +9,6 @@ export const TextField = <T extends FormikValues>(props: PropTypes<T>): JSX.Elem
   const { name } = props;
   const formik = useFormikContext<T>();
   const error = formik.touched[name] && formik.errors[name]
-  console.log(error);
   return <MuiTextField 
     error={!!error}
     helperText={typeof error === "string" ? error : JSON.stringify(error)}
