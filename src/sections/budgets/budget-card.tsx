65bda1ac243655fc8ca08dd3b4a8680b6c9a6b50
JTRB <jacobrobertsbaca@gmail.com>
2023-09-24 12:02:04 -0700

    feat: leftover tooltip on savings

--- src/sections/budgets/budget-card.tsx
+++ src/sections/budgets/budget-card.tsx
@@ -14,7 +14,7 @@ import {
   styled
 } from '@mui/material';
 
-import { Budget, BudgetSummary, CategorySummary } from 'src/types/budget/types';
+import { ActualNominal, Budget, BudgetSummary, CategorySummary } from 'src/types/budget/types';
 import { dateFormat } from 'src/types/utils/methods';
 import { moneyFormat, moneySub, moneySum } from 'src/types/money/methods';
 import { budgetSummary, budgetSummaryMerged } from 'src/types/budget/methods';
@@ -23,19 +23,10 @@ import { useCallback } from 'react';
 import { produce } from 'immer';
 import InformationCircleIcon from '@heroicons/react/24/outline/InformationCircleIcon';
 import { categoryTitle } from 'src/types/category/methods';
+import { Money } from 'src/types/money/types';
 
-const SpendingBar = (props: CategorySummary) => {
-  const { actual, nominal, type } = props;
-
-  const getColors = useCallback((theme: Theme) => {
-    if (type === CategoryType.Income)
-      return { fg: theme.palette.grey.A400, bg: theme.palette.grey.A200 };
-    if (type === CategoryType.Investments)
-      return { fg: theme.palette.info.main, bg: theme.palette.info.light };
-    if (type === CategoryType.Spending)
-      return { fg: theme.palette.warning.main, bg: theme.palette.warning.light };
-    return { fg: theme.palette.success.main, bg: theme.palette.success.lightest };
-  }, [type]);
+const SpendingBar = (props: ActualNominal) => {
+  const { actual, nominal } = props;
 
   const getValue = useCallback(() => {
     if (nominal.amount === 0) return actual.amount >= 0 ? 100 : 0;
@@ -43,17 +34,9 @@ const SpendingBar = (props: CategorySummary) => {
     return actual.amount <= 0 ? 0 : 100 * actual.amount / nominal.amount;
   }, [actual, nominal]);
 
-  const StyledProgress = styled(LinearProgress)(({ theme }) => {
-    const colors = getColors(theme);
-    return {
-      [`&.${linearProgressClasses.determinate}`]: { backgroundColor: colors.bg },
-      [`& > .${linearProgressClasses.bar1Determinate}`]: { backgroundColor: colors.fg }
-    }
-  });
-
   return (
     <Box>
-      <StyledProgress
+      <LinearProgress
         variant="determinate"
         value={getValue()}
       />
@@ -70,16 +53,50 @@ const SpendingBar = (props: CategorySummary) => {
   );
 };
 
-const TitledSpendingBar = (props: CategorySummary & { title: React.ReactNode }) => (
+type TitledSpendingBarProps = CategorySummary & {
+  title: React.ReactNode;
+  tooltip?: React.ReactNode;
+};
+
+const TitledSpendingBar = (props: TitledSpendingBarProps) => (
   <Box sx={{ mt: 2 }}>
-    <Stack direction="row" alignItems="center" spacing={0.5}>
+    <Stack direction="row" alignItems="center" spacing={0.25}>
       <Typography variant="subtitle2" color="text.secondary">
         {props.title}
       </Typography>
+      {props.tooltip && 
+        <Tooltip title={props.tooltip} placement="right">
+          <SvgIcon fontSize="inherit" color="disabled">
+            <InformationCircleIcon />
+          </SvgIcon>
+        </Tooltip>
+      }
     </Stack>
     <SpendingBar {...props} />
   </Box>
 );
+
+const LeftoverTooltip = (props: { leftovers: ActualNominal }) => {
+  const { actual, nominal } = props.leftovers;
+  
+  const MoneyText = ({ amount }: { amount: Money}) => 
+    <Typography 
+      variant="inherit" 
+      display="inline" 
+      color={amount.amount >= 0 ? "success.light" : "error.main"} 
+      fontWeight={600}>
+      &nbsp;{moneyFormat(amount)}&nbsp;
+    </Typography>
+
+  return (
+    <Typography variant="inherit">
+      <MoneyText amount={actual} />
+      leftover of
+      <MoneyText amount={nominal} />
+      planned
+    </Typography>
+  );
+};
 
 type BudgetCardProps = {
   budget: Budget
@@ -87,12 +104,6 @@ type BudgetCardProps = {
 
 export default function BudgetCard({ budget }: BudgetCardProps) {
   const { categories, leftovers } = budgetSummaryMerged(budget, CategoryType.Savings);
-  const getTitle = useCallback((summary: CategorySummary) => {
-    let title = categoryTitle(summary.type);
-    if (summary.type === CategoryType.Savings && leftovers)
-      title += " and Leftovers";
-    return title;
-  }, [leftovers]);
 
   return (
     <Grid xs={12} sm={6} md={4}>
@@ -105,7 +116,15 @@ export default function BudgetCard({ budget }: BudgetCardProps) {
             {`${dateFormat(budget.dates.begin)} — ${dateFormat(budget.dates.end)}`}
           </Typography>
           <Divider sx={{ mt: 2 }} />
-          { categories.map(s => <TitledSpendingBar key={s.type} title={getTitle(s)} {...s} />)}
+          { categories.map(s => <TitledSpendingBar 
+            key={s.type} 
+            title={categoryTitle(s.type)} 
+            tooltip={
+              s.type === CategoryType.Savings && leftovers && 
+              <LeftoverTooltip leftovers={leftovers} />
+            }
+            {...s} />
+          )}
         </CardContent>
       </Card>
     </Grid>
