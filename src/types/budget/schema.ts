c21a5a03ff8e7e733aeac759a255279f43bee06b
JTRB <jacobrobertsbaca@gmail.com>
2023-10-27 23:27:30 -0700

    fix: typescript error

--- src/types/budget/schema.ts
+++ src/types/budget/schema.ts
@@ -4,14 +4,9 @@ import { CategorySchema } from "../category/schema";
 import { datesDays } from "../utils/methods";
 import { budgetMaxDays, budgetMaxYears } from "./methods";
 
-export const BudgetSchema = z
-  .object({
-    id: z.string(),
-    name: z.string().min(1),
-    dates: DatesSchema,
-    categories: CategorySchema.array(),
-  })
-  .refine(
-    (value) => datesDays(value.dates) <= budgetMaxDays(),
-    `Budget duration cannot exceed ${budgetMaxYears()} years`
-  );
+export const BudgetSchema = z.object({
+  id: z.string(),
+  name: z.string().min(1),
+  dates: DatesSchema,
+  categories: CategorySchema.array(),
+});
