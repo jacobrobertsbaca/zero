d9033f0f8c1badd715ffa69d4c4996ade459e15e
JTRB <jacobrobertsbaca@gmail.com>
2023-10-29 18:26:37 -0700

    feat: transaction sidebar skeleton (WIP)

---
+++ src/sections/transactions/transaction-sidebar.tsx
@@ -0,0 +1,107 @@
+import { Stack, Typography } from "@mui/material";
+import { isEqual } from "lodash";
+import { useCallback } from "react";
+import { DateField } from "src/components/form/date-field";
+import { MoneyField } from "src/components/form/money-field";
+import { SelectField } from "src/components/form/select-field";
+import { TextField } from "src/components/form/text-field";
+import { Scrollbar } from "src/components/scrollbar";
+import { EditActions, EditState } from "src/components/sidebar/edit-actions";
+import { Sidebar } from "src/components/sidebar/sidebar";
+import { SidebarHeader } from "src/components/sidebar/sidebar-header";
+import { Budget } from "src/types/budget/types";
+import { categoryTitle } from "src/types/category/methods";
+import { Transaction } from "src/types/transaction/types";
+import { dateFormat } from "src/types/utils/methods";
+import * as Yup from "yup";
+
+type TransactionSidebarProps = {
+  transaction: Transaction;
+  budgets: readonly Budget[];
+  open: boolean;
+  onClose: () => void;
+};
+
+export const TransactionSidebar = ({ transaction, budgets, open, onClose }: TransactionSidebarProps) => {
+  const isExisting = !!transaction.id;
+
+  const budgetValues = budgets.map((b) => ({
+    value: b.id,
+    label: (
+      <Stack>
+        <Typography variant="body2">{b.name}</Typography>
+        <Typography variant="caption" color="text.secondary">
+          {`${dateFormat(b.dates.begin)} — ${dateFormat(b.dates.end)}`}
+        </Typography>
+      </Stack>
+    ),
+  }));
+
+  const categoryValues = useCallback(
+    (budgetId: string) => {
+      const budget = budgets.find((b) => b.id === budgetId);
+      if (!budget) return [];
+      return budget.categories.map((c) => ({
+        value: c.id,
+        label: (
+          <Stack>
+            <Typography variant="body2">{c.name}</Typography>
+            <Typography variant="caption" color="text.secondary">
+              {categoryTitle(c.type)}
+            </Typography>
+          </Stack>
+        ),
+      }));
+    },
+    [budgets]
+  );
+
+  return (
+    <Sidebar
+      open={open}
+      onClose={onClose}
+      FormProps={{
+        enableReinitialize: true,
+        initialValues: transaction,
+        validationSchema: Yup.object({
+          date: Yup.string().required("Enter a valid date!"),
+          budget: Yup.string()
+            .required("You must pick a budget!")
+            .test("has-categories", "You must pick a budget with at least one category!", (id, ctx) => {
+              return !!budgets.find((b) => b.id === id);
+            }),
+          category: Yup.string().required("You must pick a category!"),
+          amount: Yup.mixed().required("You must enter an amount!"),
+        }),
+        async onSubmit(values) {
+          console.log(values);
+        },
+      }}
+    >
+      {(form) => (
+        <>
+          <SidebarHeader onClose={onClose}>{isExisting ? "Edit Transaction" : "New Transaction"}</SidebarHeader>
+          <Scrollbar sx={{ flexGrow: 1 }}>
+            <Stack spacing={3} sx={{ p: 3 }}>
+              <DateField label="Date" name="date" />
+              <SelectField
+                label="Budget"
+                name="budget"
+                values={budgetValues}
+                onChange={(evt) => {
+                  // Reset category to none when budget changes
+                  form.setFieldValue("category", "");
+                  form.setFieldValue("budget", evt.target.value);
+                }}
+              />
+              <SelectField label="Category" name="category" values={categoryValues(form.values.budget)} />
+              <MoneyField label="Amount" name="amount" />
+              <TextField label="Name" name="name" placeholder="Optional" />
+              <EditActions allowDelete={isExisting} dirty={!isEqual(form.values, transaction)} state={EditState.Edit} />
+            </Stack>
+          </Scrollbar>
+        </>
+      )}
+    </Sidebar>
+  );
+};
