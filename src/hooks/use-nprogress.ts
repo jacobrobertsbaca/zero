1eae3afc01505ac6c5f3668c40c7be5644518589
JTRB <jacobrobertsbaca@gmail.com>
2024-09-13 02:02:02 -0600

    fix: attempt fix progress stall

--- src/hooks/use-nprogress.ts
+++ src/hooks/use-nprogress.ts
@@ -4,12 +4,11 @@ import nProgress from "nprogress";
 
 const ignoreQuery = (pathRef: MutableRefObject<string | undefined>, func: () => any) => (url: any) => {
   /** If url is not a string, then we can't do any path-dependent logic. */
-  console.log(url);
   if (typeof url === "string") {
     const path = url.split("?")[0];
     if (pathRef.current === path) return;
     pathRef.current = path;
-  }
+  } else pathRef.current = undefined;
   return func();
 };
 
@@ -18,8 +17,8 @@ export function useNProgress() {
   const endPath = useRef<string>();
 
   useEffect(() => {
-    const onRouteStart = ignoreQuery(startPath, () => { console.log("start"); nProgress.start(); });
-    const onRouteComplete = ignoreQuery(endPath, () => { console.log("end"); nProgress.done(); });
+    const onRouteStart = ignoreQuery(startPath, nProgress.start);
+    const onRouteComplete = ignoreQuery(endPath, nProgress.done);
 
     Router.events.on("routeChangeStart", onRouteStart);
     Router.events.on("routeChangeError", onRouteComplete);
